bb4275cd28c185fd155ff0d9f89913ae16ea3e06
shintiaps <shintiaputrisalamah20@gmail.com>
2026-01-19 06:12:32 +0000

    game v1.1

--- game.py
+++ game.py
@@ -6,6 +6,7 @@ guess_number = int(input("Masukkan Tebak Angka:"))
 
 while guess_number != secreat_number:
     print("Tebakan Salah, Silakan Coba Lagi")
+    print("Anda terjebak dalam perputaran abadi") #modify
     guess_number = int(input("Masukkan Tebakan Angka"))
 
 print("Selamat..!!, Tebakan anda benar!!")
